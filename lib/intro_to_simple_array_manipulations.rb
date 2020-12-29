4f5f8dc165ea65e9c9dc73b7513ad2477cdbf72c
Blaine R Love <blainelove94@gmail.com>
2020-12-29 22:34:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,3 @@
-# array[ , , , , ]
-# next = "singer"
-# array.push(next)
-#
-# array.unshift("Bob")
-#
-# array.unshift(" ")
 
 def using_push(array, string)
   array.push(string)
@@ -12,4 +5,8 @@ end
 
 def using_unshift(array, string)
   array.unshift(string)
+end
+
+def using_pop(array, string)
+  array.pop
 end
